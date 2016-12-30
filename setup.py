633432ea0e30fff43aa08ee39d50f8b0b048d7ff
Jed <jed-frey@users.noreply.github.com>
2016-12-30 12:05:15 -0500

    Git Auto Commit: Fri Dec 30 12:05:15 EST 2016

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='canape',
+      version='1.0.0',
+      description='Python CFFI for Vector CANape',
+      url='https://github.com/jedediahfrey/PyCANape',
+      author='Jed Frey',
+      author_email='canape@exstatic.org',
+      license='BSD',
+      packages=['canape'],
+      zip_safe=False)
