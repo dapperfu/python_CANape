bea65dd7c8975f84fa5ad89678fe639f5ea0d246
Jed Frey <jed-frey@users.noreply.github.com>
2018-02-08 20:25:52 -0500

    Update __init__.py

--- CANape/__init__.py
+++ CANape/__init__.py
@@ -1,6 +1,14 @@
 __VERSION__ = "1.0.0"
 
 class CANape(object):
+    """Placeholder.
+    
+    Due to NDA contracts that I signed the actual original implementation belongs to and remains at Caterpillar, Inc.
+    
+    However I've done it once and re-creating it would be trivial. If your company wants a much better high level API
+    to control CANape, my contact information is below.
+    """
+    
     def __init__(self):
         pass
     
